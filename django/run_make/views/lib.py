43c7f37f09a905c8d0f475572a3da2848d965a6f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-02-26 19:14:13 -0500

    factor out a function
      namely, factor
        def hash_from_str ( s : str ) -> str:
      out of
        def write_form_to_maybe_new_user_folder (
            users_folder : str, # a path
            form : ModelForm ):

--- django/run_make/views/lib.py
+++ django/run_make/views/lib.py
@@ -5,15 +5,20 @@ import os
 from   django.forms import ModelForm
 
 
+def hash_from_str ( s : str ) -> str:
+  return (
+      hashlib . md5 (
+        s . encode () )
+      . hexdigest () )
+
 def write_form_to_maybe_new_user_folder (
     users_folder : str, # a path
     form : ModelForm ):
   """ Completely IO; returns nothing. """
   hp = os.path.join (
        users_folder,
-       ( hashlib . md5 (
-           form . cleaned_data ["user_email"] . encode () )
-         . hexdigest () ) )
+       hash_from_str (
+           form . cleaned_data ["user_email"] ) )
   if not os.path.exists ( hp ):
       os.mkdir ( hp )
   with open ( os.path.join ( hp, 'shell.json' ),
