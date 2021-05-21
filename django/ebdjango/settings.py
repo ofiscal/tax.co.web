3020ebf29b378eece195197fc928ad3a45106156
Aaron Chock <achockco@gmail.com>
2021-05-21 07:29:18 -0600

    adding static files for styles and comments

--- django/ebdjango/settings.py
+++ django/ebdjango/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'ebdjango.urls'
 TEMPLATES = [
   {
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
+    'DIRS': [os.path.join((BASE_DIR), 'templates/')],
     'APP_DIRS': True,
     'OPTIONS': {
       'context_processors': [
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
