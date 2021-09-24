d6b36b5819adf2476178662c202470b2f96c417a
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-09-24 18:19:38 -0500

    Two easy bugfixes, and add a demo to the demo.
      Previously the demo defined the functions; now it uses them.
    
      The bugfixes were
        (1) To put the __doc__ string after, not before, the function arguments.
        (2) To include some definitions that had somehow been dropped.

--- studies/csv_fetch_tax_rates.py
+++ studies/csv_fetch_tax_rates.py
@@ -7,6 +7,8 @@ from   typing import List
 # PITFALL: These absolute paths need a '.' prepended to them,
 # in order to work with the Python `csv` library.
 # But Django wants them to be absolute.
+# They've been written Django-style to duplicate the environment
+# in which I'll be using these functions.
 marginal_rate_tables = {
   "/config/marginal_rates/most.csv" :
     "El impuesto para la mayoría de las categorías de ingreso:",
@@ -18,18 +20,17 @@ marginal_rate_tables = {
     "El impuesto más bajo para los ingresos ocasionales:",
 }
 
-mrt_filepaths = list ( marginal_rate_tables
-                       . keys () )
-
 def rate_ceiling_pairs_to_rate_floor_pairs (
+    pairs : List[ List[ float ] ]
+    ) ->    List[ List[ float ] ]:
   """
   Leaves the rates (first) column unchanged.
   Moves each ceiling down a cell,
   which means the last ceiling (which should be infinity) is lost,
   and sets the first ceiling to 0.
   """
-    pairs : List[ List[ float ] ]
-    ) ->    List[ List[ float ] ]:
+  ceiling = 1 # Just an index, for the second elt of each pair.
+  ln = len ( pairs )
   for i in range( 0, ln-1 ):
     pairs  [ ln-i-1 ] [ ceiling ] = (
       pairs[ ln-i-2 ] [ ceiling ]  )
@@ -50,5 +51,13 @@ def read_rate_ceiling_table (
     for row in reader:
       pairs . append ( [ row [ "rate" ],
                          row [ "ceiling" ] ] )
+  csvFileName = os.path.split ( csvFilePath ) [1]
   return [ csvFileName,
            rate_ceiling_pairs_to_rate_floor_pairs ( pairs ) ]
+
+mrt_filepaths = list ( marginal_rate_tables
+                       . keys () )
+rates = []
+for p in ( marginal_rate_tables . keys () ):
+  rates . append (
+    read_rate_ceiling_table ( "." + p ) )
