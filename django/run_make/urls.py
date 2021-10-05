932cc757265293a70990c720a420012f1aab574e
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-10-05 17:10:13 -0500

    improve some names

--- django/run_make/urls.py
+++ django/run_make/urls.py
@@ -1,49 +1,49 @@
 from django.urls import path
 
-import run_make.views.views    as vs
-import run_make.views.examples as vx
+import run_make.views.views    as views
+import run_make.views.examples as examples
 
 
 app_name = 'run_make'
 
 urlpatterns = [
     path( 'write_time',
-          vx.write_time,
+          examples.write_time,
           name='write_time' ),
 
     path( 'render_content_argument',
-          vx.render_content_argument,
+          examples.render_content_argument,
           name='render_content_argument' ),
 
     path( 'manual_ingest',
-          vs.manual_ingest,
+          views.manual_ingest,
           name='manual_ingest'),
 
     path( 'ingest_full_spec',
-          vs.ingest_full_spec,
+          views.ingest_full_spec,
           name='ingest_full_spec'),
 
     path( 'thank-for-spec/<user_email>',
-          vs.thank_for_spec,
+          views.thank_for_spec,
           name='thank-for-spec'),
 
     path( 'dynamic_form',
-          vx.dynamic_form,
+          examples.dynamic_form,
           name='dynamic_form'),
 
     path( 'download',
-          vx.download,
+          examples.download,
           name='download'),
 
     path( "upload_multiple_with_logging",
-          vx.upload_multiple_with_logging,
+          examples.upload_multiple_with_logging,
           name="upload_multiple_with_logging" ),
 
     path( 'upload_and_show_url',
-           vx.upload_and_show_url,
+           examples.upload_and_show_url,
            name="upload_and_show_url" ),
 
     path( 'upload_multiple',
-           vx.upload_multiple,
+           examples.upload_multiple,
            name="upload_multiple" )
 ]
